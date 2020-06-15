28ec115673a99a5da79c8ee1658c63494782795b
Kallol Ghosh <kallolghosh@FHMAC19063.local>
2020-06-15 23:36:32 +0530

    Changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # insider-assignment
 Assignment for PayTM insider
+
+Configuration Used
+1. My SQL for storing stories which can be used for Past Stories
+2. For Caching used Redis
--- src/main/java/com/insider/assignment/pojo/response/Story.java
+++ src/main/java/com/insider/assignment/pojo/response/Story.java
@@ -16,7 +16,7 @@ public class Story {
 
     private Integer score;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
     private Date timeOfSubmission;
 
     private String user;
